b503597592c62566804dce0f331d5ead63f77fa1
tayler6000 <taylerporter@gmail.com>
2022-07-19 23:38:20 -0500

    Now showing the looked up user's actual score.

---
+++ html/api/get_score.php
@@ -0,0 +1,21 @@
+<?php
+
+$json = file_get_contents("php://input");
+$data = json_decode($json, true);
+
+$data["service_name"] = "ETN";
+$data["service_key"] = getenv("ETN_SERVICE_KEY");
+
+$postdata = json_encode($data);
+$ch = curl_init("http://www.eigentrust.net:31415/get_score");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+$result = curl_exec($ch);
+http_response_code(curl_getinfo($ch, CURLINFO_RESPONSE_CODE));
+curl_close($ch);
+
+echo $result;
+
+?>
