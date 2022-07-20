5c9e5133821c9573a1ad9d10cf39b0a2a423d8dd
tayler6000 <taylerporter@gmail.com>
2022-07-19 23:17:42 -0500

    Added view user button on account page. Created view user functionality.

---
+++ html/api/get_vote_count.php
@@ -0,0 +1,20 @@
+<?php
+
+$json = file_get_contents("php://input");
+$data = json_decode($json);
+
+$data["service_name"] = "ETN";
+$data["service_key"] = getenv("ETN_SERVICE_KEY");
+
+$postdata = json_encode($data);
+$ch = curl_init("http://www.eigentrust.net:31415/get_vote_count");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json");
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
+?>
