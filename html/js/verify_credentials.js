7ec1c91d3bbad63b5af7dcbebd3ddad2e99cd75c
tayler6000 <taylerporter@gmail.com>
2022-07-31 20:21:44 -0500

    Moved the code to verify the user is logged in on every page to js/verify_credentials.js Removed code from each page and referenced js/verify_credentials.js in each page.

---
+++ html/js/verify_credentials.js
@@ -0,0 +1,44 @@
+function logout(){
+    document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "key=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "type=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "expires=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.href = "http://www.eigentrust.net/index.html";
+}
+
+// Verify logged in or redirect and die.
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var username = getCookie("username");
+var password = getCookie("key");
+var password_type = getCookie("type");
+var data = {"username": username, "password": password, "password_type": password_type};
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        return;
+    }else if(this.readyState == 4){
+        logout();
+        document.getElementsByTagName("html")[0].innerHTML = "Please Login.";
+    }
+}
+xhttp.open("POST", "http://www.eigentrust.net/api/verify_credentials.php", false);
+xhttp.setRequestHeader("Content-type", "application/json");
+xhttp.setRequestHeader("Accept", "text/plain");
+xhttp.send(JSON.stringify(data));
